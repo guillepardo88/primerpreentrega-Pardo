8957379037b3bf6cae271c7a69dc56fcffc4879a
Guillermo Pardo <guillermopardo88@gmail.com>
2023-02-06 13:25:55 -0300

    primer entrega Guillermo Pardo

---
+++ src/componentes/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../img/logo.png"
+import carrito from "../img/carrito.png"
+import header from "../componentes/estilos/Header.css"
+
+const Header = (children) => {
+    return (
+        <div className="contenedor"> 
+            <img src={logo} alt="logo tienda" />
+            <h1>Electro Boom</h1>
+            {/* {children} */}
+            <nav>
+                <ul>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </nav>
+            <img src={carrito} alt="carritoCompras" />
+            <span>0</span>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/componentes/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Nombre = () => {
+    
+    return (
+    <h1>Guillote</h1>
+    )
+}
+
